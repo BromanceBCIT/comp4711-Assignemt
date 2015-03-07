4791e4124e1936ab3e774d5538cfbcec4759894e
Clemens Lo <lsps9140814@gmail.com>
2015-03-07 00:05:07 -0800

    Edit some stuff to init Signed-off-by:Clemens Lo <lsps9140814@gmail.com>

---
+++ application/models/ContactMessages.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ContactMessages
+ *
+ * @author Clemens
+ */
+class ContactMessages extends MY_Model{
+    // Constructor
+    public function __construct() {
+        parent::__construct('contactmessages', 'contactId');
+    }
+    
+    public function getMessage($messageNum) {
+        
+        if ($messageNum <= $this->size())
+        {
+            return $this->get($messageNum);
+        }
+        else
+        {
+            return null;
+        }
+      
+    }
+    
+    public function getAllMessages() {
+        return $this->all();
+    }
+}
