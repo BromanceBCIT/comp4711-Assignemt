5a52d99a3700d387bf17fb4e0deaf2e7e5c55637
William <william781201@gmail.com>
2015-03-09 01:37:25 -0700

    step by step... Signed-off-by:William <william781201@gmail.com>

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -137,4 +137,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array('dinedata', 'homedata','aboutdata', 'Contacts', 'Photos');
+$autoload['model'] = array('Restaurants', 'homedata','aboutdata', 'Contacts', 'Photos');
