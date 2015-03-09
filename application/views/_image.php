736e2be3edbaaba08906ec027c4c0ace6250cff9
Clemens Lo <lsps9140814@gmail.com>
2015-03-08 22:10:31 -0700

    updated image view Signed-off-by:Clemens Lo <lsps9140814@gmail.com>

--- application/views/_image.php
+++ application/views/_image.php
@@ -1,10 +1,12 @@
 <div class="imageView">
-    <h1><a href="{photo}"></a>{title}</h1>    
-    <p>{description}</p>
+    <h1><a href="{photo}"></a>{title}</h1>
+    <br> </br>
     <a class="largeImage" href="{photo}"
    data-title="&quot;{description}&quot; by {author}">
     <img src="{photo}" />
     </a>
+    <p>{description}</p>
+    <br> </br>
     <p>
      {author} Uploaded on: {postDate}
     </p>
